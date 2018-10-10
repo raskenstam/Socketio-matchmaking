f0af825ff5499cbe79cacbeb6f90f76906b6e8a9
Unknown <mangastens@gmail.com>
2018-10-10 23:24:01 +0200

    Fixed

--- index.js
+++ index.js
@@ -10,39 +10,50 @@ var con = mysql.createConnection({
 });
 
 */
-var currentlobbies = [[]];
-
+var currentlobbies=new Array(10)
+for (i=0; i <9; i++){
+currentlobbies[i]=new Array(10)
+}
 var newobject = {
-  name: "name",id:"id "
+  name: "name", id: "id "
 
 }
-var i=0;
-var ii=0;
+
 var port = process.env.PORT || 3000;
 //todo startfunction if a server is ready reserve a place and check if user is ready. if user is ready add them to servertable.
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-
-io.on('connection', function(socket){
-  socket.on('a', function(msg){
+io.on('connection', function (socket) {
+  socket.on('a', function (msg) {
     io.emit('welcome', msg);
-    addusertoarray(newobject);
+    addusertoarray("sure");
   });
 });
-
-http.listen(port, function(){
+http.listen(port, function () {
   console.log('listening on *:' + port);
 });
-function addusertoarray(object){
-  a = i+10;
-  while ( i < a) { 
-    currentlobbies[i[ii]] = object;
-    console.log(currentlobbies[i[ii]]);
+function addusertoarray(object) {
+  var i = 0;
+  var a = 0;
+  while (i < 9) {
+    a = 0;
+    while (a < 9) {
+      
+      if(currentlobbies[i][a]==undefined){
+        console.log(currentlobbies[i[a]]);
+        currentlobbies[i][a]=object;
+        return;
+      }
+      else{
+        console.log("defined"+currentlobbies[i][a]+""+a);
+      }
+      a++
+    }
     i++;
-    
-}
-  
+
+  }
+
 }
 /*function getfromdatabase(sql) {
 con.connect(function(err) {
